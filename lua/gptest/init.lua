d3b7946fee27a904852fc57fbdccf98546b1294e
AlexanderMirchev <alexim@abv.com>
2023-05-20 18:43:06 +0300

    Added visual text selection

--- lua/gptest/init.lua
+++ lua/gptest/init.lua
@@ -1,1 +1,11 @@
-require('gptest.request')
+-- require('gptest.request')
+
+local function test()
+  local selectedText = require('gptest.select').get_selected_text()
+  print(selectedText)
+
+end
+
+local gptest = {}
+gptest.test = test
+return gptest
---
+++ lua/gptest/select.lua
@@ -0,0 +1,29 @@
+local select = {}
+
+local function get_selected_text()
+  local vstart = vim.fn.getpos("'<")
+  local vend = vim.fn.getpos("'>")
+
+  local line_start = vstart[2]
+  local line_end = vend[2]
+
+  -- get all lines
+  local lines = vim.api.nvim_buf_get_lines(0, line_start-1, line_end, false)
+
+  -- remove text before the first line start
+  lines[1] = string.sub(lines[1], vstart[3], -1)
+  if #lines > 1 then
+    -- remove text after the last line end
+    lines[#lines] = string.sub(lines[#lines], 1, vend[3])
+  else
+    -- remove text after the last line end
+    -- however consider that the end will be shifted by the start
+    lines[1] = string.sub(lines[1], 1, vend[3] - vstart[3] + 1)
+  end
+
+  return table.concat(lines, '\n')
+end
+
+select.get_selected_text = get_selected_text
+
+return select
